750f0f95e6b96c6dfa7d66e91112025b09be86b0
es5es5 <es5e5s@nate.com>
2023-05-23 16:06:17 +0900

    parallax-background DONE

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,8 @@ var canvas = document.getElementById('canvas1');
 var ctx = canvas.getContext('2d');
 var CANVAS_WIDTH = (canvas.width = 800);
 var CANVAS_HEIGHT = (canvas.height = 700);
-var gameSpeed = 15;
+var gameSpeed = 10;
+var gameFrame = 0;
 var backgroundLayer1 = new Image();
 var backgroundLayer2 = new Image();
 var backgroundLayer3 = new Image();
@@ -13,20 +14,39 @@ backgroundLayer2.src = './assets/images/layer-2.png';
 backgroundLayer3.src = './assets/images/layer-3.png';
 backgroundLayer4.src = './assets/images/layer-4.png';
 backgroundLayer5.src = './assets/images/layer-5.png';
-var x = 0;
-var x2 = 2400;
+var Layer = /** @class */ (function () {
+    function Layer(image, speedModifier) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+    Layer.prototype.update = function () {
+        this.speed = gameSpeed * this.speedModifier;
+        this.x = (gameFrame * this.speed) % this.width;
+    };
+    Layer.prototype.draw = function () {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    };
+    return Layer;
+}());
+var layer1 = new Layer(backgroundLayer1, 0.2);
+var layer2 = new Layer(backgroundLayer2, 0.4);
+var layer3 = new Layer(backgroundLayer3, 0.6);
+var layer4 = new Layer(backgroundLayer4, 0.8);
+var layer5 = new Layer(backgroundLayer5, 1);
+var gameObjects = [layer1, layer2, layer3, layer4, layer5];
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if (x < -2400)
-        x = 2400 + x2 - gameSpeed;
-    else
-        x -= gameSpeed;
-    if (x2 < -2400)
-        x2 = 2400 + x - gameSpeed;
-    else
-        x2 -= gameSpeed;
+    gameObjects.forEach(function (object) {
+        object.update();
+        object.draw();
+    });
+    gameFrame--;
     requestAnimationFrame(animate);
 }
 animate();
--- docs/app.ts
+++ docs/app.ts
@@ -2,8 +2,8 @@ const canvas = document.getElementById('canvas1') as HTMLCanvasElement
 const ctx = canvas.getContext('2d')!
 const CANVAS_WIDTH = (canvas.width = 800)
 const CANVAS_HEIGHT = (canvas.height = 700)
-
-let gameSpeed = 15
+let gameSpeed = 10
+let gameFrame = 0
 
 const backgroundLayer1 = new Image() as HTMLImageElement
 const backgroundLayer2 = new Image() as HTMLImageElement
@@ -16,17 +16,58 @@ backgroundLayer3.src = './assets/images/layer-3.png'
 backgroundLayer4.src = './assets/images/layer-4.png'
 backgroundLayer5.src = './assets/images/layer-5.png'
 
-let x = 0
-let x2 = 2400
+class Layer {
+  x: number
+  y: number
+  width: number
+  height: number
+  x2: number
+  image: HTMLImageElement
+  speedModifier: number
+  speed: number
+
+  constructor(image, speedModifier) {
+    this.x = 0
+    this.y = 0
+    this.width = 2400
+    this.height = 700
+    this.image = image
+    this.speedModifier = speedModifier
+    this.speed = gameSpeed * this.speedModifier
+  }
+
+  update() {
+    this.speed = gameSpeed * this.speedModifier
+    this.x = (gameFrame * this.speed) % this.width
+  }
+
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    ctx.drawImage(
+      this.image,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height,
+    )
+  }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.2)
+const layer2 = new Layer(backgroundLayer2, 0.4)
+const layer3 = new Layer(backgroundLayer3, 0.6)
+const layer4 = new Layer(backgroundLayer4, 0.8)
+const layer5 = new Layer(backgroundLayer5, 1)
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5]
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  ctx.drawImage(backgroundLayer4, x, 0)
-  ctx.drawImage(backgroundLayer4, x2, 0)
-  if (x < -2400) x = 2400 + x2 - gameSpeed
-  else x -= gameSpeed
-  if (x2 < -2400) x2 = 2400 + x - gameSpeed
-  else x2 -= gameSpeed
+  gameObjects.forEach((object) => {
+    object.update()
+    object.draw()
+  })
+  gameFrame--
   requestAnimationFrame(animate)
 }
 
